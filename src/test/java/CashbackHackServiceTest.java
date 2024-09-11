64eebe093f5c6654f7d73c5f117bcd9403ea6901
Samohin Sergei <Bravochief21@gmail.com>
2024-09-11 08:18:23 +0500

    Adding a falling test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -36,6 +36,15 @@ public class CashbackHackServiceTest {
         int expected = 1000;
         assertEquals(actual, expected);
     }
+    @Test
+    public void buyAnother1000 (){
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected =0;
+
+        assertEquals(actual,expected);
+    }
 
 
 }
